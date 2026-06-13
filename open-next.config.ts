b28c24734275c2d620bc31ad41753577830bf66e
fwrkzn <furkan@zerosix.be>
2026-06-13 15:06:07 +0200

    Remove KV incremental cache requirement

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache
-});
+export default defineCloudflareConfig({});
